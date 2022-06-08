2fe7f72c6d97a032de1bfdf324c4b0d2c84fb94d
Michel Podkhvatilin <michel.podkhvatilin@gmail.com>
2022-06-08 21:17:53 +0300

    add bulma

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { Recipe } from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent { }
+export class RecipeListComponent {
+  recipes: Recipe[] = [];
+}
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
-  styleUrls: ['./recipes.component.ts']
+  styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent { }
